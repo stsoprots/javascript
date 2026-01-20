4926105936ff99c9c0f1c536967cc0b676d74bad
stsoprots <stso.prots@gmail.com>
2026-01-20 19:55:29 +0200

    lesson 11

---
+++ Lesson 11/hw11_1/main.js
@@ -0,0 +1,48 @@
+// #HmvAfRQM
+//
+// – взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+
+let cartsDiv = document.getElementById('carts');
+fetch('https://dummyjson.com/carts')
+    .then(value => value.json())
+    .then(cartsObject => {
+        let {carts} = cartsObject; //деструктуризація
+        console.log(carts);
+
+
+        for (let cart of carts) {
+            let div = document.createElement('div');
+            div.classList.add('cart-container');
+            let divWithInfo = document.createElement('div')
+            divWithInfo.innerText = `
+                "total": ${cart.total},
+                "discountedTotal": ${cart.discountedTotal},
+                "userId": ${cart.userId},
+                "totalProducts": ${cart.totalProducts},
+                "totalQuantity": ${cart.totalQuantity}
+            `;
+
+            let ol = document.createElement('ol');
+            for (const product of cart.products) {
+                let li = document.createElement('li');
+                let info = document.createElement('p')
+                info.innerText = `
+                    "id": ${product.id},
+                    "title": ${product.title},
+                    "price": ${product.price},
+                    "quantity": ${product.quantity},
+                    "total": ${product.total},
+                    "discountPercentage": ${product.discountPercentage},
+                    "discountedTotal": ${product.discountedTotal}
+                `
+                let img = document.createElement('img');
+                img.src = product.thumbnail;
+                li.append(img, info);
+                ol.appendChild(li);
+            }
+
+            div.append(divWithInfo, ol);
+            cartsDiv.appendChild(div);
+        }
+    });
+
